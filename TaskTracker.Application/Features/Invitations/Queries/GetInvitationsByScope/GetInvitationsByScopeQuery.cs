3e85bfbcd184ebbdb62f2b247193ef6a037d75bc
Salon <rautsalon@gmail.com>
2026-04-29 11:31:06 +0545

    fix: Added IAuthorizedRequest so it involves in authorization pipeline

--- TaskTracker.Application/Features/Invitations/Queries/GetInvitationsByScope/GetInvitationsByScopeQuery.cs
+++ TaskTracker.Application/Features/Invitations/Queries/GetInvitationsByScope/GetInvitationsByScopeQuery.cs
@@ -1,11 +1,21 @@
 using MediatR;
+using TaskTracker.Application.Authorization;
 using TaskTracker.Application.DTOs;
+using TaskTracker.Domain.Constants;
 using TaskTracker.Domain.Enums;
 
 namespace TaskTracker.Application.Features.Invitations.Queries.GetInvitationsByScope;
 
-public sealed class GetInvitationsByScopeQuery : IRequest<IReadOnlyList<InvitationDto>>
+public sealed class GetInvitationsByScopeQuery : IRequest<IReadOnlyList<InvitationDto>>, IAuthorizedRequest
 {
     public ScopeType ScopeType { get; set; }
     public Guid ScopeId { get; set; }
+
+    public string RequiredPermission => AppPermissions.InvitationsView;
+    public IReadOnlyList<ResourceScope> Scopes => ScopeType switch
+    {
+        ScopeType.Organization => [new ResourceScope(ResourceType.Organization, ScopeId)],
+        ScopeType.Project => [new ResourceScope(ResourceType.Project, ScopeId)],
+        _ => []
+    };
 }
--- TaskTracker.Application/Features/Members/Queries/GetMembersByScope/GetMembersByScopeQuery.cs
+++ TaskTracker.Application/Features/Members/Queries/GetMembersByScope/GetMembersByScopeQuery.cs
@@ -1,11 +1,21 @@
 using MediatR;
+using TaskTracker.Application.Authorization;
 using TaskTracker.Application.DTOs;
+using TaskTracker.Domain.Constants;
 using TaskTracker.Domain.Enums;
 
 namespace TaskTracker.Application.Features.Members.Queries.GetMembersByScope;
 
-public sealed class GetMembersByScopeQuery : IRequest<ScopeMembersDto>
+public sealed class GetMembersByScopeQuery : IRequest<ScopeMembersDto>, IAuthorizedRequest
 {
     public ScopeType ScopeType { get; set; }
     public Guid ScopeId { get; set; }
+
+    public string RequiredPermission => AppPermissions.MembersView;
+    public IReadOnlyList<ResourceScope> Scopes => ScopeType switch
+    {
+        ScopeType.Organization => [new ResourceScope(ResourceType.Organization, ScopeId)],
+        ScopeType.Project => [new ResourceScope(ResourceType.Project, ScopeId)],
+        _ => []
+    };
 }
